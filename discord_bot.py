e0509fd91615b96e6ac2d55cde5a581b60a91722
Henderson Harper <hw_harper@hotmail.com>
2026-09-01 12:02:18 -0400

    added new functions

--- discord_bot.py
+++ discord_bot.py
@@ -12,24 +12,35 @@ async def on_ready():
 
 stocks = bot.create_group("stocks", "Commands related to stock trading")
 
-@stocks.command(name="runalgorithm", description="Run the open today algorithm and send the results to Discord.")
+@stocks.command(name="runalgorithm", description="Run the open today algorithm.")
 async def runalgoritm(ctx):
     await ctx.defer()  # Acknowledge the command to avoid timeout
     result = stocks_functions.openTodayAlgo()
     await ctx.respond(result)
 
-@stocks.command(name="closepositions", description="Close all open positions and send the results to Discord.")
+@stocks.command(name="closepositions", description="Close all open positions.")
 async def closepositions(ctx):
     await ctx.defer()  # Acknowledge the command to avoid timeout
     result = stocks_functions.closeAllPositions()
     await ctx.respond(result)
 
-@stocks.command(name="getcurrentpositions", description="Get all current positions and send the results to Discord.")
+@stocks.command(name="checkaccount", description="Get all current positions and account balance.")
 async def getcurrentpositions(ctx):
     await ctx.defer()  # Acknowledge the command to avoid timeout
     result = stocks_functions.checkPositions()
     await ctx.respond(result)
-    
+
+@stocks.command(name="lookupsymbol", description="Lookup a stock symbol.")
+async def lookupsymbol(ctx, symbol: str):
+    await ctx.defer()  # Acknowledge the command to avoid timeout
+    result = stocks_functions.lookupSymbol(symbol)
+    await ctx.respond(result)
+
+@stocks.command(name="buy", description="Buy a stock symbol.")
+async def buy(ctx, symbol: str, size: int):
+    await ctx.defer()  # Acknowledge the command to avoid timeout
+    result = stocks_functions.openPosition(symbol, size)
+    await ctx.respond(result)
 
 try:
     bot.run(discord_token)
--- stocks_functions.py
+++ stocks_functions.py
@@ -81,7 +81,7 @@ def openTodayAlgo():
     print("Opened positions:")
     for symbol, price in symbol_price_dict.items():
         size = int(curBalance / price / len(symbol_price_dict))
-        db_functions.open_new_position(symbol, size, price)
+        db_functions.open_position(symbol, size, price)
         print(f"  {symbol}: {size} shares at ${price}")
         retInfo += f"- Opened position for {symbol}: {size} shares at ${price}\n"
 
@@ -89,7 +89,7 @@ def openTodayAlgo():
 
 def closeAllPositions():
     retInfo = "## CLOSING ALL POSITIONS:\n"
-    oldPositions = db_functions.get_open_positions()
+    oldPositions = db_functions.get_current_positions()
     if not oldPositions:
         print("No current positions to close.")
         retInfo += "No current positions to close.\n"
@@ -105,7 +105,7 @@ def closeAllPositions():
             "APCA-API-SECRET-KEY": alpaca_secret_key
         }
         response = requests.get(url, headers=headers).json()["bars"]
-        assert type(response) == dict[str, dict[str, float]], "Response from Alpaca API is not in the expected format."
+        assert type(response) == dict, "Response from Alpaca API is not in the expected format."
 
         for position in oldPositions:
             symbol = position['symbol']
@@ -127,26 +127,84 @@ def closeAllPositions():
 
 def checkPositions():
     retInfo = "## CURRENT POSITIONS:\n"
-    oldPositions = db_functions.get_open_positions()
-    if not oldPositions:
+    openPositions = db_functions.get_current_positions()
+    if not openPositions:
         print("No current positions.")
         retInfo += "No current positions.\n"
     else:
-        print("Old positions:")
+        print("Open positions:")
         # Fetch the latest bar data for each symbol
-        url = f"https://data.alpaca.markets/v2/stocks/bars/latest?symbols={','.join([record['symbol'] for record in oldPositions])}"
+        url = f"https://data.alpaca.markets/v2/stocks/bars/latest?symbols={','.join([record['symbol'] for record in openPositions])}"
         headers = {
             "accept": "application/json",
             "APCA-API-KEY-ID": alpaca_api_key,
             "APCA-API-SECRET-KEY": alpaca_secret_key
         }
         response = requests.get(url, headers=headers).json()["bars"]
-        assert type(response) == dict[str, dict[str, float]], "Response from Alpaca API is not in the expected format."
-        for position in oldPositions:
+        assert type(response) == dict, "Response from Alpaca API is not in the expected format."
+        for position in openPositions:
             symbol = position['symbol']
             curprice = response[symbol]['c']  # Get the current price from the latest bar data
             delta_percent = round(((curprice - position['openprice']) / position['openprice']) * 100, 2)
             print(f"  {position['symbol']}: {position['size']} shares at ${position['openprice']} | (current price: ${curprice}, change: {delta_percent}%)")
             retInfo += f"- {position['symbol']}: {position['size']} shares at ${position['openprice']} | (current price: ${curprice}, change: {delta_percent}%)"
 
+    curBalance = db_functions.get_current_balance()
+    assert curBalance is not None, "Current balance is None. Please check the database."
+    curBalance = round(curBalance['balance'], 2)
+    retInfo += f"\n\n## CURRENT BALANCE:\n- ${curBalance}\n"
+    return retInfo
+
+def lookupSymbol(symbol):
+    symbol = symbol.upper()
+    url = f"https://data.alpaca.markets/v2/stocks/bars/latest?symbols={symbol}"
+    headers = {
+        "accept": "application/json",
+        "APCA-API-KEY-ID": alpaca_api_key,
+        "APCA-API-SECRET-KEY": alpaca_secret_key
+    }
+    response = requests.get(url, headers=headers).json()["bars"]
+    try:
+        print(response)
+        assert type(response) == dict, "Response from Alpaca API is not in the expected format."
+    except AssertionError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response}")
+        return f"Error: Unable to retrieve data for symbol {symbol}. Please check the symbol and try again."
+    retInfo = f"""## SYMBOL LOOKUP: {symbol}
+    Current price: ${response[symbol]['c']}
+    Open price: ${response[symbol]['o']}
+    High price: ${response[symbol]['h']}
+    Low price: ${response[symbol]['l']}
+    Percent change since open: {round(((response[symbol]['c'] - response[symbol]['o']) / response[symbol]['o']) * 100, 2)}%
+    """
+    return retInfo
+
+def openPosition(symbol, size):
+    symbol = symbol.upper()
+    url = f"https://data.alpaca.markets/v2/stocks/bars/latest?symbols={symbol}"
+    headers = {
+        "accept": "application/json",
+        "APCA-API-KEY-ID": alpaca_api_key,
+        "APCA-API-SECRET-KEY": alpaca_secret_key
+    }
+    response = requests.get(url, headers=headers).json()["bars"]
+    try:
+        assert type(response) == dict, "Response from Alpaca API is not in the expected format."
+    except AssertionError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response}")
+        return f"Error: Unable to retrieve data for symbol {symbol}. Please check the symbol and try again."
+    
+    price = response[symbol]['c']  # Get the current price from the latest bar data
+    db_functions.open_position(symbol, size, price)
+    retInfo = f"Opened position for {symbol}: {size} shares at ${price}"
+    try:
+        curBalance = db_functions.get_current_balance()
+        assert curBalance is not None, "Current balance is None. Please check the database."
+        curBalance = round(curBalance['balance'], 2)
+        retInfo += f"\n\n## CURRENT BALANCE:\n- ${curBalance}\n"
+    except AssertionError as e:
+        print(f"Error: {e}")
+        retInfo += "\n\n## CURRENT BALANCE:\n- Error retrieving current balance. Please check the database.\n"
     return retInfo
